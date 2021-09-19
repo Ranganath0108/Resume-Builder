a3dcfb876b6840a3fad8ec73f93149ef5075c46b
Ranganath0108 <ranganathswamy.ys@gmail.com>
2021-09-19 19:26:08 +0530

    essentials added

---
+++ js/signup.js
@@ -0,0 +1,8 @@
+function ShowPass() {
+    let x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
